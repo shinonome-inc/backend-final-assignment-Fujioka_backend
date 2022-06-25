2fbaee3ff240815a988c0e826c026bcf737a5309
fujinomoto62 <derutarou@gmail.com>
2022-06-25 16:38:12 +0900

    test

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,10 +4,8 @@ from .models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-
 class CreateForm(UserCreationForm):
-    
+
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2')
-
--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+
 @login_required
 def testfunc(request):
     return render(request, 'tweets/test.html')
