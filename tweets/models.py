5c78c02b951b404fdfafaf0b147745551d00a068
fujinomoto62 <derutarou@gmail.com>
2022-07-01 18:44:00 +0900

    Apply foreign key to tweet author

--- tweets/models.py
+++ tweets/models.py
@@ -1,12 +1,15 @@
+from tkinter import CASCADE
 from django.db import models
 from django.utils import timezone
+
+from accounts.models import User
 
 
 class TweetModel(models.Model):
     text = models.TextField(max_length=140)
     good = models.IntegerField(null=True, blank=True, default=0)
     bad = models.IntegerField(null=True, blank=True, default=0)
-    author = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField(null=True, blank=True, default=timezone.now)
     good_user_list = models.TextField(null=True, blank=True, default='')
     bad_user_list = models.TextField(null=True, blank=True, default='')
