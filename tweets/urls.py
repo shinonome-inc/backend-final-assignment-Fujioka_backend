11ea2157ca7d9cae1daae7b8e789dc4e20cd5cfe
fujinomoto62 <derutarou@gmail.com>
2022-07-01 16:14:45 +0900

    only delete service

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from tweets.views import TweetCreate, detailfunc, listfunc
+from tweets.views import TweetCreate, TweetDelete, delete_confirmfunc, detailfunc, listfunc
 
 
 app_name = 'tweets'
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', listfunc, name='list'),
     path('create/', TweetCreate.as_view(), name='create'),
     path('detail/<int:pk>', detailfunc, name='detail'),
+    path('delete_confirm/<int:pk>', delete_confirmfunc, name='delete_confirm'),
+    path('delete/<int:pk>', TweetDelete.as_view(), name='delete')
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 
 from tweets.models import TweetModel
+from accounts.models import User
 
 
 @login_required
@@ -25,5 +26,24 @@ class TweetCreate(CreateView):
     
 @login_required
 def detailfunc(request, pk):
+    client_user = request.user.get_username()
+    author = request.GET['author']
+    identity = True if client_user == author else False
     tweet = get_object_or_404(TweetModel, pk=pk)
-    return render(request, 'tweets/detail.html', {'tweet': tweet})
+    context = {
+        'tweet': tweet,
+        'identity': identity,
+    }
+    return render(request, 'tweets/detail.html', context)
+
+@login_required
+def delete_confirmfunc(request, pk):
+    client_user = request.user.get_username()
+    author = request.GET['author']
+    if client_user == author:
+        return redirect('tweets:delete', pk = pk)
+
+class TweetDelete(DeleteView):
+    model = TweetModel
+    template_name = 'tweets/delete.html'
+    success_url = reverse_lazy('tweets:list')
