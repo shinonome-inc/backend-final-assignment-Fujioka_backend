9c5205bd47f093db97402e0f9dce9b2e58994014
fujinomoto62 <derutarou@gmail.com>
2022-07-18 00:47:13 +0900

    create follow model and profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import signupfunc
+from .views import profilefunc, signupfunc
 
 
 app_name = 'accounts'
 urlpatterns = [
     path('signup/', signupfunc, name='signup'),
+    path('profile/<int:user_pk>', profilefunc, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+
+from accounts.models import User
+from tweets.models import TweetModel
 
 from .forms import CreateForm
 
@@ -18,3 +22,36 @@ def signupfunc(request):
     else:
         form = CreateForm()
     return render(request, 'accounts/signup.html', {'form': form})
+
+
+@login_required
+def profilefunc(request, user_pk):
+    # 使用しているユーザと、クリックされたユーザを取得
+    client_user = request.user
+    account = get_object_or_404(User, pk=user_pk)
+    # これらのユーザが同一人物家を確認
+    identity = True if client_user == account else False
+    
+    # フォローしているかの確認
+    follow_detecting = False
+    if (identity == False) :
+        follow_detecting = True if account in client_user.following_user.values_list('following_user', flat=True) else False
+    
+
+    # ツイートの取得
+    try:
+        user_tweets = get_list_or_404(TweetModel.objects.order_by(
+            'created_date').reverse().all(), author=account)
+    except:
+        user_tweets = False
+    tweets_exist = True if user_tweets else False
+
+    context = {
+        'account': account,
+        'user_tweets': user_tweets,
+        'tweets_exist': tweets_exist,
+        'identify': identity,
+        'follow_detecting': follow_detecting,
+    }
+
+    return render(request, 'accounts/profile.html', context)
