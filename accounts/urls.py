9426c4c49b8d3c26a7208a5647e3338b9225203d
fujinomoto62 <derutarou@gmail.com>
2022-07-18 16:27:33 +0900

    create following json response

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import profilefunc, signupfunc
+from .views import profilefunc, signupfunc, followfunc
 
 
 app_name = 'accounts'
 urlpatterns = [
     path('signup/', signupfunc, name='signup'),
     path('profile/<int:user_pk>', profilefunc, name='profile'),
+    path('follow', followfunc, name='follow'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
+import json
+from typing import TypedDict
+from django.http import JsonResponse
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 
-from accounts.models import User
+from accounts.models import FollowModel, User
 from tweets.models import TweetModel
 
 from .forms import CreateForm
@@ -35,7 +38,7 @@ def profilefunc(request, user_pk):
     # フォローしているかの確認
     follow_detecting = False
     if (identity == False) :
-        follow_detecting = True if account in client_user.following_user.values_list('following_user', flat=True) else False
+        follow_detecting = True if account.pk in client_user.following_user.values_list('follower_user', flat=True) else False
     
 
     # ツイートの取得
@@ -59,3 +62,49 @@ def profilefunc(request, user_pk):
     }
 
     return render(request, 'accounts/profile.html', context)
+
+
+@login_required
+def followfunc(request):
+    # JSからのPOST受け取り
+    if request.method == 'POST':
+        json_body = request.body.decode("utf-8")
+        body = json.loads(json_body)
+        # postの中に適切なデータが入っているか確認
+        if body['client_pk'] and body['account_pk']:
+            client = get_object_or_404(User, pk=body['client_pk'])
+            account = get_object_or_404(User, pk=body['account_pk'])
+            
+            # フォローしているかの再確認
+            follow_detecting = True if account.pk in client.following_user.values_list('follower_user', flat=True) else False
+
+            dataType = TypedDict('dataType', {'client': User, 'account': User})
+            data: dataType = dict(client = client, account = account)
+            # フォローしていたときの処理
+            if follow_detecting:
+                FollowHandle.delete_followfunc(data)
+            # フォローしていなかったときの処理
+            else:
+                FollowHandle.create_followfunc(data)
+
+            context = {
+                'is_followed': not follow_detecting,
+            }
+            return JsonResponse(context)
+        
+        else:
+            # postの内容物が揃っていなかったときのエラーハンドリング
+            pass
+    
+
+class FollowHandle():
+    dataType = TypedDict('dataType', {'client': User, 'account': User})
+    
+    # フォローしていたときの処理
+    def delete_followfunc(data: dataType):
+        follow = get_object_or_404(FollowModel, following_user=data['client'], follower_user=data['account'])
+        follow.delete()
+
+    # フォローしていなかったときの処理
+    def create_followfunc(data: dataType):
+        FollowModel.objects.create(following_user=data['client'], follower_user=data['account'])
