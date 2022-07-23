25361aada5099c49aea693a2a13d22737e21b0a1
fujinomoto62 <derutarou@gmail.com>
2022-07-23 21:37:00 +0900

    create unique constraint

--- accounts/models.py
+++ accounts/models.py
@@ -9,5 +9,13 @@ class User(AbstractUser):
 
 
 class FollowModel(models.Model):
-    following_user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, related_name='following_user', unique=True)
-    follower_user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, related_name='follower_user', unique=True)
+    following_user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, related_name='following_user')
+    follower_user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, related_name='follower_user')
+    
+    class Meta:
+        constraints = {
+            models.UniqueConstraint(
+                fields=['following_user', 'follower_user'],
+                name='follow_combination',
+            ),
+        }
